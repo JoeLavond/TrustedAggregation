bf1cae5e66dd6a46422b5b06b000ccde137e2848
JoeLavond <joelavond@gmail.com>
2022-07-19 21:19:33 -0400

    all scaling options

--- article/figures.py
+++ article/figures.py
@@ -4,7 +4,18 @@ import matplotlib.pyplot as plt
 
 
 # helper
-def scaling(x, c):
+def sq_scaling(x, c):
+
+    temp = np.array([y * np.log(y) if y > 0 else 0 for y in x])
+    temp /= ((1/c) * np.log((1/c)))
+    temp[temp > 2] = 2
+    out = 1 - (1 - temp) ** 2
+
+    print(out)
+    return out
+
+
+def abs_scaling(x, c):
 
     temp = np.array([y * np.log(y) if y > 0 else 0 for y in x])
     temp /= ((1/c) * np.log((1/c)))
@@ -32,42 +43,81 @@ def main():
     x = np.linspace(start=0, stop=1, num=200)
 
     # scaling coefs
-    c2 = scaling(x, c=2)
-    c5 = scaling(x, c=5)
-    c10 = scaling(x, c=10)
-    c15 = scaling(x, c=15)
+    sq_c2 = sq_scaling(x, c=2)
+    sq_c5 = sq_scaling(x, c=5)
+    sq_c10 = sq_scaling(x, c=10)
+    sq_c15 = sq_scaling(x, c=15)
 
     fig, axarr = plt.subplots(ncols=4, sharey=True, figsize=(16, 4))
 
     plt.sca(axarr[0])
-    plt.plot(x, c2)
+    plt.plot(x, sq_c2)
     plt.ylim(0, 1.1)
     plt.vlines((1 / 2), 0, 1, 'g', 'dashed')
     plt.text((1 / 2), 1.05, 'balanced', c='g')
     plt.title('D = 2')
 
     plt.sca(axarr[1])
-    plt.plot(x, c5)
+    plt.plot(x, sq_c5)
     plt.ylim(0, 1.1)
     plt.vlines((1 / 5), 0, 1, 'g', 'dashed')
     plt.text((1 / 5), 1.05, 'balanced', c='g')
     plt.title('D = 5')
 
     plt.sca(axarr[2])
-    plt.plot(x, c10)
+    plt.plot(x, sq_c10)
     plt.ylim(0, 1.1)
     plt.vlines((1 / 10), 0, 1, 'g', 'dashed')
     plt.text((1 / 10), 1.05, 'balanced', c='g')
     plt.title('D = 10')
 
     plt.sca(axarr[3])
-    plt.plot(x, c15)
+    plt.plot(x, sq_c15)
     plt.ylim(0, 1.1)
     plt.vlines((1 / 15), 0, 1, 'g', 'dashed')
     plt.text((1 / 15), 1.05, 'balanced', c='g')
     plt.title('D = 15')
 
-    plt.savefig('scaling.png')
+    plt.savefig('sq_scaling.png')
+
+
+    # scaling coefs
+    abs_c2 = abs_scaling(x, c=2)
+    abs_c5 = abs_scaling(x, c=5)
+    abs_c10 = abs_scaling(x, c=10)
+    abs_c15 = abs_scaling(x, c=15)
+
+    fig, axarr = plt.subplots(ncols=4, sharey=True, figsize=(16, 4))
+
+    plt.sca(axarr[0])
+    plt.plot(x, abs_c2)
+    plt.ylim(0, 1.1)
+    plt.vlines((1 / 2), 0, 1, 'g', 'dashed')
+    plt.text((1 / 2), 1.05, 'balanced', c='g')
+    plt.title('D = 2')
+
+    plt.sca(axarr[1])
+    plt.plot(x, abs_c5)
+    plt.ylim(0, 1.1)
+    plt.vlines((1 / 5), 0, 1, 'g', 'dashed')
+    plt.text((1 / 5), 1.05, 'balanced', c='g')
+    plt.title('D = 5')
+
+    plt.sca(axarr[2])
+    plt.plot(x, abs_c10)
+    plt.ylim(0, 1.1)
+    plt.vlines((1 / 10), 0, 1, 'g', 'dashed')
+    plt.text((1 / 10), 1.05, 'balanced', c='g')
+    plt.title('D = 10')
+
+    plt.sca(axarr[3])
+    plt.plot(x, abs_c15)
+    plt.ylim(0, 1.1)
+    plt.vlines((1 / 15), 0, 1, 'g', 'dashed')
+    plt.text((1 / 15), 1.05, 'balanced', c='g')
+    plt.title('D = 15')
+
+    plt.savefig('abs_scaling.png')
 
     """ Previously """
     old_c2 = old_scaling(x, c=2)
