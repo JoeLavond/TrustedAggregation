8931ff82c620a71011d1fc8429d905b7e2f77959
JoeLavond <joelavond@gmail.com>
2022-07-30 14:25:48 -0400

    add gitignore

--- article/99_visuals/run.sh
+++
@@ -1,4 +0,0 @@
-python experiment_figures.py --n_rounds=75 --n_malicious=4 --m_start=25;
-python experiment_figures.py --alpha=1 --n_rounds=75 --n_malicious=4 --m_start=25;
-python experiment_figures.py --alpha_val=1 --n_rounds=75 --n_malicious=4 --m_start=25;
-python experiment_figures.py --alpha=1 --alpha_val=1 --n_rounds=75 --n_malicious=4 --m_start=25;
