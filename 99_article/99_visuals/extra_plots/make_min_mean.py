497bc5d7fb8650c09be0b3899d9b07cdc854bc14
JoeLavond <joelavond@gmail.com>
2022-08-16 10:51:51 -0400

    visuals for comparison with sota defenses

---
+++ 99_article/99_visuals/extra_plots/make_min_mean.py
@@ -0,0 +1,187 @@
+""" Packages """
+# base
+import argparse
+import numpy as np
+import os
+import re
+
+# smoothing
+from statsmodels.tsa.holtwinters import SimpleExpSmoothing, ExponentialSmoothing
+
+# visual
+import matplotlib
+#matplotlib.use('agg')
+import matplotlib.pyplot as plt
+#matplotlib.pyplot.switch_backend('agg')
+import seaborn as sns
+
+
+def get_args():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--n_rounds', default=250, type=int)
+    parser.add_argument('--d_rounds', default=50, type=int)
+    parser.add_argument('--alpha', default=10000, type=int)
+    parser.add_argument('--alpha_val', default=10000, type=int)
+
+    return parser.parse_args()
+
+
+""" Helper functions """
+def get_thresh(x):
+
+    out = []
+    for i in range(len(x)):
+        temp = x[:(i + 1)]
+        out.append(2 * np.mean(temp[np.argmin(temp):]))
+
+    return out
+
+def get_quantiles(r, values, running=0, global_min=0):
+
+    u = np.unique(r)
+    u.sort()
+
+    q1, q3 = [], []
+    for i in u:
+
+        if running:
+            index = (r <= i)
+            temp = values[index]
+
+            if global_min:
+
+                # find min of values in this or previous round
+                min_loc = np.argmin(temp)
+                min_r = r[index][min_loc]
+
+                index = np.logical_and(
+                    index, r >= min_r  # occur since global min
+                )
+                temp = values[index]
+
+        else:
+            index = (r == i)
+            temp = values[index]
+
+        # calculate quantiles
+        temp_l = np.quantile(temp, .25)
+        q1.append(temp_l)
+        temp_u = np.quantile(temp, .75)
+        q3.append(temp_u)
+
+    return np.array(q1), np.array(q3)
+
+
+def plot_threshold(
+    data_val, data_user, d_rounds,  # ----- input data and rounds to display
+    suffix, path='.'  # ------------------- .png output location
+    ):
+
+    # validation user
+    data_val, data_user = np.array(data_val), np.array(data_user)
+    data_val = data_val[:-1]  # remove last validation check for next iteration never run
+    data_val, scaling = data_val[1:], data_val[0, 1:]  # remove scaling row, remove round from scaling
+    data_val_r, data_val_values = data_val[:, 0], data_val[:, 1:]  # seperate round from data
+
+    data_val_max = data_val_values.max(axis=1)
+    data_val_max_thresh = get_thresh(data_val_max)
+    data_val_max_thresh = np.maximum(np.minimum(data_val_max_thresh, 1), 0)
+
+    # benign users
+    data_benign = data_user[data_user[:, 0] == 0, 1:]  # subset to benign users, remove malicious index column
+    data_benign_r, data_benign_values = data_benign[:, 0], data_benign[:, 1:]  # seperate round column from data
+    data_benign_max = data_benign_values.max(axis=1)
+
+    data_benign_max_q1, data_benign_max_q3 = get_quantiles(data_benign_r, data_benign_max)
+    benign_upper = data_benign_max_q3 + 1.5 * (data_benign_max_q3 - data_benign_max_q1)
+    benign_upper = np.maximum(np.minimum(benign_upper, 1), 0)
+    benign_lower = data_benign_max_q1 - 1.5 * (data_benign_max_q3 - data_benign_max_q1)
+    benign_lower = np.maximum(np.minimum(benign_lower, 1), 0)
+
+    fig, [ax1, ax2] = plt.subplots(ncols=2, sharey=True, figsize=(8, 4))
+    plt.suptitle('Comparison of Various Thresholding Methods')
+    ax1.set_title('Global-Min Smoothing')
+
+    ax1.set_xlabel('Communication Round')
+    ax1.set_xlim(0, d_rounds)
+    ax1.set_ylim(-0.05, 1.1)
+
+    ax1.plot(data_val_r + 1, benign_upper, '--b')
+    ax1.plot(data_val_r + 1, benign_lower, '--b')
+
+    ax1.plot(data_val_r + 1, 2*data_val_max, '-k')
+    ax1.plot(data_val_r + 1, data_val_max_thresh, '-g')
+
+    ax1.legend(labels=[
+        'benign round upper: q3 + 1.5 * IQR',
+        'benign round lower: q1 - 1.5 * IQR',
+        '2 * val round',
+        'min-mean val max ks'
+    ])
+
+    # smoothing
+    simple_exp = SimpleExpSmoothing(2 * data_val_max, initialization_method="estimated").fit()
+    exp_smooth = ExponentialSmoothing(2 * data_val_max, trend='mul', initialization_method="estimated").fit()
+
+    ax2.set_title('Traditional Smoothing')
+
+    ax2.set_xlabel('Communication Round')
+    ax2.set_xlim(0, d_rounds)
+    ax2.set_ylim(-0.05, 1.1)
+
+    ax2.plot(data_val_r + 1, benign_upper, '--b')
+    ax2.plot(data_val_r + 1, benign_lower, '--b')
+    ax2.plot(data_val_r + 1, 2 * data_val_max, '-k')
+    ax2.plot(data_val_r + 1, simple_exp.fittedvalues, '-y')
+    ax2.plot(data_val_r + 1, exp_smooth.fittedvalues, '-r')
+
+    ax2.legend(labels=[
+        'benign round upper: q3 + 1.5 * IQR',
+        'benign round lower: q1 - 1.5 * IQR',
+        '2 * val round',
+        'simple exponential smoothing',
+        'exponential smoothing',
+    ])
+
+    if path is not None:
+        plt.savefig(os.path.join(path, f'min_mean{suffix}--d_rounds{d_rounds}.png'), bbox_inches='tight')
+
+    #plt.show()
+    plt.close()
+
+
+""" Main function HERE """
+def main():
+
+    # setup
+    args = get_args()
+    if args.d_rounds is None:
+        args.d_rounds = args.n_rounds
+
+    path = os.path.join(
+        '/home/joe/03_federated/01_trusted/centralized',
+        'alpha' + str(args.alpha) + '--alpha_val' + str(args.alpha_val)
+    )
+    suffix = '--alpha' + str(args.alpha) + '--alpha_val' + str(args.alpha_val)
+
+    if not os.path.exists(os.path.join(path, 'visuals')):
+        os.mkdir(path, 'visuals')
+
+
+    """ Defense Plot - Thresholding """
+    # import no attack nor defense data
+    subdir = os.path.join(
+        path,
+        'n_rounds' + str(args.n_rounds) + '--d_start1--m_start1--n_malicious1'
+    )
+
+    temp_val = np.load(os.path.join(subdir, 'data/output_val_ks.npy'), allow_pickle=True)
+    temp_user = np.load(os.path.join(subdir, 'data/output_user_ks.npy'), allow_pickle=True)
+
+    plot_threshold(temp_val, temp_user, args.d_rounds, suffix)
+
+
+if __name__ == "__main__":
+    main()
+
