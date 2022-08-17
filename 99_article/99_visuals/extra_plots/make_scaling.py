450e1899e3649aa3aef4a0ad261297e23ed887d9
JoeLavond <joelavond@gmail.com>
2022-08-17 15:39:32 -0400

    modify scaling file

--- 99_article/99_visuals/extra_plots/make_scaling.py
+++ 99_article/99_visuals/extra_plots/make_scaling.py
@@ -16,65 +16,95 @@ def scaling(x, c):
     return out
 """
 
-def scaling(x, num_classes):
-    c = 1 / np.exp(1)
+def trunc_linear(x, num_classes):
 
-    out = np.zeros_like(x)
-    for i, y in enumerate(x):
-        if y == 0:
-            pass
-        else:
+    B = 1 / num_classes
 
-            if y <= c:
-                out[i] = y * np.log(y)
-            else:
-                out[i] = 2 * c * np.log(c) - y * np.log(y)
+    index = (x <= 2 * B)
+    output = np.zeros_like(x)
+    output[index] = (B - np.abs(B - x[index])) / B
 
-    out /= ((1 / num_classes) * np.log((1 / num_classes)))
-    #temp[temp > 2] = 2
-    out = 1 - np.abs((1 - out))
+    return output
 
-    return out
 
+def trunc_entropy(x, num_classes):
+
+    B = 1 / num_classes
+
+    index = (x <= 2 * B)
+    output = np.zeros_like(x)
+    output[index] = (B - np.abs(B - x[index]))
+    output = np.array([y * np.log(y) if y > 0 else 0 for y in output])
+    output /= (B * np.log(B))
+
+    return output
+
+
+def scale_all(x, num_classes, entropy=0):
+
+    B = 1 / num_classes
+
+    index = (x > B)
+    output = x.copy()
+    output[index] = B * (output[index] - B) / (1 - B) + B
+
+    return trunc_entropy(output, num_classes) if entropy else trunc_linear(output, num_classes)
 
 
 """ Main function HERE """
 def main():
 
     x = np.linspace(start=0, stop=1, num=200)
-
-    # scaling coefs
-    abs_c3 = scaling(x, 3)
-    abs_c5 = scaling(x, 5)
-    abs_c10 = scaling(x, 10)
-    abs_c15 = scaling(x, 15)
+    plt.plot(x, scale_all(x, 3))
+    plt.plot(x, scale_all(x, 5))
+    plt.plot(x, scale_all(x, 10))
 
     fig, axarr = plt.subplots(ncols=4, sharey=True, figsize=(16, 4))
 
     plt.sca(axarr[0])
-    plt.plot(x, abs_c3)
+    plt.plot(x, trunc_linear(x, 3))
+    plt.plot(x, trunc_entropy(x, 3))
+    plt.plot(x, scale_all(x, 3))
+    plt.plot(x, scale_all(x, 3, entropy=1))
+    plt.legend(labels=['trunc-lin', 'trunc-ent', 'scale-lin', 'scale-ent'])
     plt.ylim(0, 1.1)
+    plt.xlim(0, 1)
     plt.vlines((1 / 3), 0, 1, 'g', 'dashed')
     plt.text((1 / 3), 1.05, 'balanced', c='g')
     plt.title('D = 3')
 
     plt.sca(axarr[1])
-    plt.plot(x, abs_c5)
+    plt.plot(x, trunc_linear(x, 5))
+    plt.plot(x, trunc_entropy(x, 5))
+    plt.plot(x, scale_all(x, 5))
+    plt.plot(x, scale_all(x, 5, entropy=1))
+    plt.legend(labels=['trunc-lin', 'trunc-ent', 'scale-lin', 'scale-ent'])
     plt.ylim(0, 1.1)
+    plt.xlim(0, 1)
     plt.vlines((1 / 5), 0, 1, 'g', 'dashed')
     plt.text((1 / 5), 1.05, 'balanced', c='g')
     plt.title('D = 5')
 
     plt.sca(axarr[2])
-    plt.plot(x, abs_c10)
+    plt.plot(x, trunc_linear(x, 10))
+    plt.plot(x, trunc_entropy(x, 10))
+    plt.plot(x, scale_all(x, 10))
+    plt.plot(x, scale_all(x, 10, entropy=1))
+    plt.legend(labels=['trunc-lin', 'trunc-ent', 'scale-lin', 'scale-ent'])
     plt.ylim(0, 1.1)
+    plt.xlim(0, 1)
     plt.vlines((1 / 10), 0, 1, 'g', 'dashed')
     plt.text((1 / 10), 1.05, 'balanced', c='g')
     plt.title('D = 10')
 
     plt.sca(axarr[3])
-    plt.plot(x, abs_c15)
+    plt.plot(x, trunc_linear(x, 15))
+    plt.plot(x, trunc_entropy(x, 15))
+    plt.plot(x, scale_all(x, 15))
+    plt.plot(x, scale_all(x, 15, entropy=1))
+    plt.legend(labels=['trunc-lin', 'trunc-ent', 'scale-lin', 'scale-ent'])
     plt.ylim(0, 1.1)
+    plt.xlim(0, 1)
     plt.vlines((1 / 15), 0, 1, 'g', 'dashed')
     plt.text((1 / 15), 1.05, 'balanced', c='g')
     plt.title('D = 15')
