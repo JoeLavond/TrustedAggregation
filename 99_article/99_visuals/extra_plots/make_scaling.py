48fe1a911e8432275d99cb91e490ebda63ce9a42
JoeLavond <joelavond@gmail.com>
2022-08-17 18:55:27 -0400

    update scaling figure

--- 99_article/99_visuals/extra_plots/make_scaling.py
+++ 99_article/99_visuals/extra_plots/make_scaling.py
@@ -19,39 +19,14 @@ def scaling(x, num_classes):
 def main():
 
     x = np.linspace(start=0, stop=1, num=200)
-    fig, axarr = plt.subplots(ncols=4, sharey=True, figsize=(16, 4))
 
-    plt.sca(axarr[0])
-    plt.plot(x, scaling(x, 2))
-    plt.ylim(0, 1.1)
-    plt.xlim(0, 1)
-    plt.vlines((1 / 2), 0, 1, 'g', 'dashed')
-    plt.text((1 / 2), 1.05, 'balanced', c='g')
-    plt.title('D = 2')
-
-    plt.sca(axarr[1])
+    plt.figure()
     plt.plot(x, scaling(x, 3))
     plt.ylim(0, 1.1)
     plt.xlim(0, 1)
     plt.vlines((1 / 3), 0, 1, 'g', 'dashed')
     plt.text((1 / 3), 1.05, 'balanced', c='g')
     plt.title('D = 3')
-
-    plt.sca(axarr[2])
-    plt.plot(x, scaling(x, 5))
-    plt.ylim(0, 1.1)
-    plt.xlim(0, 1)
-    plt.vlines((1 / 5), 0, 1, 'g', 'dashed')
-    plt.text((1 / 5), 1.05, 'balanced', c='g')
-    plt.title('D = 5')
-
-    plt.sca(axarr[3])
-    plt.plot(x, scaling(x, 10))
-    plt.ylim(0, 1.1)
-    plt.xlim(0, 1)
-    plt.vlines((1 / 10), 0, 1, 'g', 'dashed')
-    plt.text((1 / 10), 1.05, 'balanced', c='g')
-    plt.title('D = 10')
 
     plt.savefig('scaling.png')
     plt.show()
