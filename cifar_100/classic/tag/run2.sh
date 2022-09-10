efd4b680989b1b6f97a24d0a2e51178b09150b06
Joe Lavond <joelavond@gmail.com>
2022-09-10 14:11:30 +0000

    need to run neuro experiments on cifar100

--- cifar_100/classic/tag/run2.sh
+++ cifar_100/classic/tag/run2.sh
@@ -1,11 +1,10 @@
 source activate joeenv;
 
-python train.py --n_rounds=50 --alpha=10000 --alpha_val=10000 --d_start=51 --m_start=51 --dba=0 --n_malicious=1;
-python train.py --n_rounds=50 --alpha=10000 --alpha_val=10000 --d_start=1 --m_start=51 --dba=0 --n_malicious=1;
+CUDA_VISIBLE_DEVICES=2,3 python train.py --n_rounds=50 --alpha=10000 --alpha_val=10000 --d_start=51 --m_start=1 --dba=0 --n_malicious=1;
+CUDA_VISIBLE_DEVICES=2,3 python train.py --n_rounds=50 --alpha=10000 --alpha_val=10000 --d_start=1 --m_start=1 --dba=0 --n_malicious=1;
 
-python train.py --n_rounds=50 --alpha=10000 --alpha_val=10000 --d_start=51 --m_start=51 --dba=1 --n_malicious=2 --col_size=2;
-python train.py --n_rounds=50 --alpha=10000 --alpha_val=10000 --d_start=1 --m_start=51 --dba=1 --n_malicious=2 --col_size=2;
+CUDA_VISIBLE_DEVICES=2,3 python train.py --n_rounds=50 --alpha=10000 --alpha_val=10000 --d_start=51 --m_start=1 --dba=1 --n_malicious=2 --col_size=2;
+CUDA_VISIBLE_DEVICES=2,3 python train.py --n_rounds=50 --alpha=10000 --alpha_val=10000 --d_start=1 --m_start=1 --dba=1 --n_malicious=2 --col_size=2;
 
-python train.py --n_rounds=50 --alpha=10000 --alpha_val=10000 --d_start=51 --m_start=51 --dba=1 --n_malicious=4 --col_size=2 --row_size=2;
-python train.py --n_rounds=50 --alpha=10000 --alpha_val=10000 --d_start=1 --m_start=51 --dba=1 --n_malicious=4 --col_size=2 --row_size=2;
-
+CUDA_VISIBLE_DEVICES=2,3 python train.py --n_rounds=50 --alpha=10000 --alpha_val=10000 --d_start=51 --m_start=1 --dba=1 --n_malicious=4 --col_size=2 --row_size=2;
+CUDA_VISIBLE_DEVICES=2,3 python train.py --n_rounds=50 --alpha=10000 --alpha_val=10000 --d_start=1 --m_start=1 --dba=1 --n_malicious=4 --col_size=2 --row_size=2;
