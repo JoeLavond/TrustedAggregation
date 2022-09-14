8e72fd66107360c631f6b3a37de5621be446791c
JoeLavond <joelavond@gmail.com>
2022-09-14 09:48:00 -0400

    redo all visuals to have appropriate names for article

--- make_article/make_visuals/run_exp.sh
+++ make_article/make_visuals/run_exp.sh
@@ -1,17 +1,17 @@
 source activate joeenv;
 
-python exp_accuracy_clean.py --show=0 --n_classes=100 --n_rounds=100 --alpha=10000 --alpha_val=10000 --dba=0 --n_malicious=1 --data='cifar' --neuro=1;
-python exp_accuracy_pois.py --show=0 --n_classes=100 --n_rounds=100 --alpha=10000 --alpha_val=10000 --dba=0 --n_malicious=1 --data='cifar' --neuro=1;
-python exp_threshold.py --show=0 --n_classes=100 --n_rounds=100 --alpha=10000 --alpha_val=10000 --dba=0 --n_malicious=1 --data='cifar' --neuro=1;
+python exp_accuracy_clean.py --show=0 --n_classes=10 --n_rounds=100 --alpha=10000 --alpha_val=10000 --dba=0 --n_malicious=1 --data='stl' --neuro=1;
+python exp_accuracy_pois.py --show=0 --n_classes=10 --n_rounds=100 --alpha=10000 --alpha_val=10000 --dba=0 --n_malicious=1 --data='stl' --neuro=1;
+python exp_threshold.py --show=0 --n_classes=10 --n_rounds=100 --alpha=10000 --alpha_val=10000 --dba=0 --n_malicious=1 --data='stl' --neuro=1;
 echo 'done'
 
-python exp_accuracy_clean.py --show=0 --n_classes=100 --n_rounds=100 --alpha=10000 --alpha_val=10000 --dba=1 --n_malicious=2 --data='cifar' --neuro=1;
-python exp_accuracy_pois.py --show=0 --n_classes=100 --n_rounds=100 --alpha=10000 --alpha_val=10000 --dba=1 --n_malicious=2 --data='cifar' --neuro=1;
-python exp_threshold.py --show=0 --n_classes=100 --n_rounds=100 --alpha=10000 --alpha_val=10000 --dba=1 --n_malicious=2 --data='cifar' --neuro=1;
+python exp_accuracy_clean.py --show=0 --n_classes=10 --n_rounds=100 --alpha=10000 --alpha_val=10000 --dba=1 --n_malicious=2 --data='stl' --neuro=1;
+python exp_accuracy_pois.py --show=0 --n_classes=10 --n_rounds=100 --alpha=10000 --alpha_val=10000 --dba=1 --n_malicious=2 --data='stl' --neuro=1;
+python exp_threshold.py --show=0 --n_classes=10 --n_rounds=100 --alpha=10000 --alpha_val=10000 --dba=1 --n_malicious=2 --data='stl' --neuro=1;
 echo 'done'
 
-python exp_accuracy_clean.py --show=0 --n_classes=100 --n_rounds=100 --alpha=10000 --alpha_val=10000 --dba=1 --n_malicious=4 --data='cifar' --neuro=1;
-python exp_accuracy_pois.py --show=0 --n_classes=100 --n_rounds=100 --alpha=10000 --alpha_val=10000 --dba=1 --n_malicious=4 --data='cifar' --neuro=1;
-python exp_threshold.py --show=0 --n_classes=100 --n_rounds=100 --alpha=10000 --alpha_val=10000 --dba=1 --n_malicious=4 --data='cifar' --neuro=1;
+python exp_accuracy_clean.py --show=0 --n_classes=10 --n_rounds=100 --alpha=10000 --alpha_val=10000 --dba=1 --n_malicious=4 --data='stl' --neuro=1;
+python exp_accuracy_pois.py --show=0 --n_classes=10 --n_rounds=100 --alpha=10000 --alpha_val=10000 --dba=1 --n_malicious=4 --data='stl' --neuro=1;
+python exp_threshold.py --show=0 --n_classes=10 --n_rounds=100 --alpha=10000 --alpha_val=10000 --dba=1 --n_malicious=4 --data='stl' --neuro=1;
 echo 'done'
 
