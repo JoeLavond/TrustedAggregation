10f0d96b98e05e2eb566a4be836edce55fa6dab9
Joe Lavond <joelavond@gmail.com>
2022-11-11 13:09:15 +0000

    attempted several runs, need to rerun on s1

--- stl_10/classic/base/run1.sh
+++
@@ -1,6 +0,0 @@
-source activate joeenv;
-
-python train.py --n_rounds=250 --alpha=10000 --alpha_val=10000 --trim_mean=0 --beta=.2 --dba=0 --n_malicious=1;
-python train.py --n_rounds=250 --alpha=10000 --alpha_val=10000 --trim_mean=0 --beta=.2 --dba=1 --n_malicious=2 --col_size=12;
-python train.py --n_rounds=250 --alpha=10000 --alpha_val=10000 --trim_mean=0 --beta=.2 --dba=1 --n_malicious=4 --col_size=12 --row_size=12;
-
