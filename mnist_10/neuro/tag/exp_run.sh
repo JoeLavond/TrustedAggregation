c13d6c7b79fb8c08a80d0d477d056d915574a247
Joe Lavond <joelavond@gmail.com>
2024-09-07 17:44:33 +0000

    runs in progress, conda env tracked

--- mnist_10/neuro/tag/exp_run.sh
+++ mnist_10/neuro/tag/exp_run.sh
@@ -1,14 +1,14 @@
-screen -dmS mnist-one bash -c "
+screen -dmS nt-mnist-one bash -c "
     source activate tag;
     python train.py --n_rounds=250 --n_malicious=1 --gpu_start=0;
 "
 
-screen -dmS mnist-two bash -c "
+screen -dmS nt-mnist-two bash -c "
     source activate tag;
     python train.py --n_rounds=250 --dba=1 --n_malicious=2 --gpu_start=1
 "
 
-screen -dmS mnist-four bash -c "
+screen -dmS nt-mnist-four bash -c "
     source activate tag;
     python train.py --n_rounds=250 --dba=1 --n_malicious=4 --gpu_start=2
 "
--- stl_10/neuro/tag/run.sh
+++ stl_10/neuro/tag/run.sh
@@ -1,2 +1,5 @@
-source activate tag;
-CUDA_VISIBLE_DEVICES=3 python train.py --n_rounds=250 --n_malicious=1 --col_size=12 --row_size=12 --d_scale=2;
+screen -dmS stl10-tag-one bash -c """
+    source activate tag;
+    python train.py --n_rounds=250 --n_malicious=1 --col_size=12 --row_size=12 --d_scale=2;
+"""
+
