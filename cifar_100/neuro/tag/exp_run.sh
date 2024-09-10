3364c358fc827374fbe0584ab1cae249b3973cc6
Joe Lavond <joelavond@gmail.com>
2024-09-10 17:18:31 +0000

    try cifar100 with smaller d_scale

--- cifar_100/neuro/tag/exp_run.sh
+++ cifar_100/neuro/tag/exp_run.sh
@@ -1,15 +1,15 @@
-screen -dmS cifar-one bash -c "
+screen -dmS cifar-one-one-five bash -c "
     source activate tag;
-    python train.py --n_rounds=250 --n_malicious=1 --alpha=1
+    python train.py --n_rounds=250 --n_malicious=1 --alpha=1 --d_scale=1.5
 "
 
-screen -dmS cifar-two bash -c "
+screen -dmS cifar-two-one-five bash -c "
     source activate tag;
-    python train.py --n_rounds=250 --dba=1 --n_malicious=2 --alpha=1
+    python train.py --n_rounds=250 --dba=1 --n_malicious=2 --alpha=1 --d_scale=1.5
 "
 
-screen -dmS cifar-four bash -c "
+screen -dmS cifar-four-one-five bash -c "
     source activate tag;
-    python train.py --n_rounds=250 --dba=1 --n_malicious=4 --alpha=1
+    python train.py --n_rounds=250 --dba=1 --n_malicious=4 --alpha=1 --d_scale=1.5
 "
 
