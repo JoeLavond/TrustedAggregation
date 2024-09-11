ce683c78c159ebba962865941cb9f8ceac08e535
Joe Lavond <joelavond@gmail.com>
2024-09-11 01:20:17 +0000

    save current start future cifar100 and stl10 imbalanced experiments

--- cifar_100/neuro/tag/exp_run.sh
+++ cifar_100/neuro/tag/exp_run.sh
@@ -1,15 +1,15 @@
 screen -dmS cifar-one-one-five bash -c "
     source activate tag;
-    python train.py --n_rounds=250 --n_malicious=1 --alpha=1 --d_scale=1.5
+    python train.py --n_rounds=250 --n_malicious=1 --alpha=1 --d_scale=1.25 --gpu_start=2
 "
 
 screen -dmS cifar-two-one-five bash -c "
     source activate tag;
-    python train.py --n_rounds=250 --dba=1 --n_malicious=2 --alpha=1 --d_scale=1.5
+    python train.py --n_rounds=250 --dba=1 --n_malicious=2 --alpha=1 --d_scale=1.25 --gpu_start=2
 "
 
 screen -dmS cifar-four-one-five bash -c "
     source activate tag;
-    python train.py --n_rounds=250 --dba=1 --n_malicious=4 --alpha=1 --d_scale=1.5
+    python train.py --n_rounds=250 --dba=1 --n_malicious=4 --alpha=1 --d_scale=1.25 --gpu_start=2
 "
 
