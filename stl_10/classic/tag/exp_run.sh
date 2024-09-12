7887dabcf4e851dd69a118894e30695a8a158377
JoeLavond <joelavond@gmail.com>
2024-09-12 09:56:59 -0400

    stl10 runs without tuning or neurotoxin, start stl10 runs with smaller scaling

--- stl_10/classic/tag/exp_run.sh
+++ stl_10/classic/tag/exp_run.sh
@@ -1,15 +1,15 @@
 screen -dmS stl-classic-one bash -c "
     source activate tag;
-    python train.py --n_rounds=250 --n_malicious=1 --alpha=1 --gpu_start=2
+    python train.py --n_rounds=250 --n_malicious=1 --gpu_start=2
 "
 
 screen -dmS stl-classic-two bash -c "
     source activate tag;
-    python train.py --n_rounds=250 --dba=1 --n_malicious=2 --alpha=1 --gpu_start=2
+    python train.py --n_rounds=250 --dba=1 --n_malicious=2 --gpu_start=2
 "
 
 screen -dmS stl-classic-four bash -c "
     source activate tag;
-    python train.py --n_rounds=250 --dba=1 --n_malicious=4 --alpha=1 --gpu_start=2
+    python train.py --n_rounds=250 --dba=1 --n_malicious=4 --gpu_start=2
 "
 
